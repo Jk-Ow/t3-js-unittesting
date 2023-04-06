7a407a18f659a1133877c2d61cd344b0eecb7f67
eri-krieg-22 <eri.krieg.22@lehre.mosbach.dhbw.de>
2023-04-06 16:33:05 +0200

    fixing some syntax

--- unittest_uebung/buggyCode.js
+++ unittest_uebung/buggyCode.js
@@ -1,14 +1,17 @@
-export function summe(a,b){
+export function addition(a,b){
     return a + b;
 }
 
-export function differenz(a,b){
+export function subtraction(a,b){
     return a - b;
 }
 
-export function potenz(a,b){
-    if(a = 0){
+export function power(a,b){
+    if(b == 0){
         return 1;
     }
     for(let i = b, )
+    {
+
+    }
 }
