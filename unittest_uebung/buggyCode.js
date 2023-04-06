09477560f97e6eb562ac56780a72e4a59aa785df
jannik <118477717+Jk-Ow@users.noreply.github.com>
2023-04-06 16:42:48 +0200

    added power() function

--- unittest_uebung/buggyCode.js
+++ unittest_uebung/buggyCode.js
@@ -7,11 +7,16 @@ export function subtraction(a,b){
 }
 
 export function power(a,b){
+    if (b < 0){
+        b = 0;
+    }
     if(b == 0){
         return 1;
     }
-    for(let i = b, )
+    let result = a;
+    for(let i = 0; i <= b; i++)
     {
-
+        result = result * a;
     }
+    return result;
 }
