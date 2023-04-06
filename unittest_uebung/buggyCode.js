68e96219b15ab903296fa09b52f33e9b6ad36993
eri-krieg-22 <eri.krieg.22@lehre.mosbach.dhbw.de>
2023-04-06 16:37:15 +0200

    adding bugs

--- unittest_uebung/buggyCode.js
+++ unittest_uebung/buggyCode.js
@@ -1,16 +1,16 @@
 export function addition(a,b){
-    return a + b;
+    return a - b;
 }
 
 export function subtraction(a,b){
-    return a - b;
+    return a + b;
 }
 
 export function power(a,b){
     if(b == 0){
         return 1;
     }
-    for(let i = b, )
+    for(let i = b)
     {
 
     }
