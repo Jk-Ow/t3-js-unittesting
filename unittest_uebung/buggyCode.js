fd89c1fc5ec031f02a90f4c92122c7d0347597b2
jannik <118477717+Jk-Ow@users.noreply.github.com>
2023-04-06 16:22:45 +0200

    Added functions summe differenz and potenz

--- unittest_uebung/buggyCode.js
+++ unittest_uebung/buggyCode.js
@@ -1,5 +1,14 @@
-export summe;
-
-function summe(a,b){
+export function summe(a,b){
     return a + b;
+}
+
+export function differenz(a,b){
+    return a - b;
+}
+
+export function potenz(a,b){
+    if(a = 0){
+        return 1;
+    }
+    for(let i = b, )
 }
