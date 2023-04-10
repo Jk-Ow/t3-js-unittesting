30930303290049886892fc02f7afdf668850454b
jannik <118477717+Jk-Ow@users.noreply.github.com>
2023-04-10 02:44:50 +0200

    added spec for subtraction and power

--- unittest_uebung/spec/CalculateSpec.js
+++ unittest_uebung/spec/CalculateSpec.js
@@ -9,4 +9,16 @@ describe('Calculate', function() {
     expect(addition(va,vb)).toEqual(va+vb);
   });
 
+  it('should be able to subtract value a from value b', function(){
+    va = 7;
+    vb = 4;
+    expect(subtraction(va,vb)).toEqual(va-vb);
+  });
+
+  it('should be able to potentiate value a with value b',function(){
+    va = 8;
+    vb = 2;
+    expect(power(va,vb)).toEqual(Math.pow(va,vb));
+  });
+
 });
